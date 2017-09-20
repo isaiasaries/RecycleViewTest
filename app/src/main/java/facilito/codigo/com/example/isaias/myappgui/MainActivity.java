d29699d251125a96a2a1e6ef0ff1f33c8c39da3e
isaiasaries <isaiasaries@hotmail.com>
2017-09-19 21:37:49 -0400

    Fist Commit in GIT

---
+++ app/src/main/java/facilito/codigo/com/example/isaias/myappgui/MainActivity.java
@@ -0,0 +1,13 @@
+package facilito.codigo.com.example.isaias.myappgui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
